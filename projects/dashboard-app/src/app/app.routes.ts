7e8f965360abce32489033e64266d97f7bbfee9e
Dnyanesh Mali <dnyanmali@gmail.com>
2026-05-02 23:49:52 +0530

    chat bot component

--- projects/dashboard-app/src/app/app.routes.ts
+++ projects/dashboard-app/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ChatbotComponent } from './chat-bot/chat-bot.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: DashboardComponent
+    },
+    {
+        path: 'chatbot',
+        component: ChatbotComponent
     }
 ];
